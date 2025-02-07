e47d7a824b37d4a654f48351416b983f10c96f2b
ikotome <kotomizu.0728@gmail.com>
2025-02-07 20:36:11 +0900

    マイグレーションファイルのカラムにnull制約を追加し、データ整合性を向上

--- db/migrate/20250206104701_create_logs.rb
+++ db/migrate/20250206104701_create_logs.rb
@@ -1,12 +1,12 @@
 class CreateLogs < ActiveRecord::Migration[8.0]
   def change
     create_table :logs do |t|
-      t.date :date_time
-      t.integer :client_type
-      t.integer :log_type
-      t.text :location
-      t.text :ip
-      t.text :user_agent
+      t.date :date_time, null: false        # DateTime: 日時
+      t.integer :client_type, null: false   # ClientType: 人間/bot/その他
+      t.integer :log_type, null: false      # LogType: 詳細ビュー/エラー/その他
+      t.text :location, null: false         # Location: 場所
+      t.text :ip, null: false               # IP: IPアドレス
+      t.text :user_agent, null: false       # UserAgent: ユーザーエージェント
 
       t.timestamps
     end
--- db/migrate/20250206110016_create_authorities.rb
+++ db/migrate/20250206110016_create_authorities.rb
@@ -1,12 +1,12 @@
 class CreateAuthorities < ActiveRecord::Migration[8.0]
   def change
     create_table :authorities do |t|
-      t.text :invitation_id
-      t.text :password
-      t.date :effective_date
-      t.integer :vaild_counts
-      t.integer :accept
-      t.integer :used_times
+      t.text :invitation_id, null: false    # Invitation : 招待ID
+      t.text :password, null: false         # Password : パスワードハッシュ/ソルト
+      t.date :effective_date, null: false   # EffectiveDate : 招待リンクの有効期限
+      t.integer :valid_counts, null: false  # ValidCounts : 有効回数
+      t.integer :accept, null: false        # Accept : 許可したいダウンロード番号
+      t.integer :used_times, null: false    # UsedTimes : 使用回数
 
       t.timestamps
     end
